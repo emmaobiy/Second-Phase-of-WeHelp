bea3d5899fccfbe539be079696c8ca5b6f38d441
emma <obiy10@gmail.com>
2024-06-09 11:44:44 +0800

    Added static file routing in app.py, homepage HTML content in index.html, and new files: index.css, attractions_handler.js, mrtlist_handler.js, and related image files.

---
+++ static/attractions_handler.js
@@ -0,0 +1,89 @@
+let page=0;
+let keyword='';
+
+
+async function fetchAttractions(){
+    const response=await fetch(`/api/attractions?page=${page}&keyword=${keyword}`);
+    const data=await response.json();
+    addAttraction(data.data);
+    if (data.nextPage===null){
+        window.removeEventListener('scroll', handleScroll);
+    } else {
+        page=data.nextPage;
+    }
+}
+
+function addAttraction(attractions) {
+    const Container=document.getElementById('attractions-container');
+
+    for (let i=0; i < attractions.length; i++) {
+        const attraction=attractions[i];
+        
+        // each-attraction
+        const eachAttraction=document.createElement('div');
+        eachAttraction.classList.add('each-attraction');
+        
+        // attraction-imgContainer
+        const imgContainer=document.createElement('div');
+        imgContainer.classList.add('attraction-imgContainer');
+        
+        // img
+        const img=document.createElement('img');
+        img.src=attraction.images[0];
+        img.alt=attraction.name;
+       
+        // attraction-name
+        const name=document.createElement('div');
+        name.classList.add('attraction-name', 'font-body-bold16-white');
+        name.textContent=attraction.name;
+
+        imgContainer.appendChild(name);
+        imgContainer.appendChild(img);
+        eachAttraction.appendChild(imgContainer);
+
+        // attraction-info
+        const info=document.createElement('div');
+        info.classList.add('attraction-info');
+        
+        const mrt=document.createElement('div');
+        mrt.classList.add('info-mrt','font-body-med16-gray');
+        mrt.textContent=attraction.mrt;
+        
+        const cat=document.createElement('div');
+        cat.classList.add('info-cat', 'font-body-med16-gray');
+        cat.textContent=attraction.cat;
+       
+        info.appendChild(mrt);
+        info.appendChild(cat);
+        eachAttraction.appendChild(info);
+
+        Container.appendChild(eachAttraction);
+    };
+}
+
+function handleScroll(){
+    const scrollTop=(document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    const scrollHeight=(document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
+    const clientHeight=document.documentElement.clientHeight || window.innerHeight;
+    const scrolledToBottom=Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+    if (scrolledToBottom) {
+        fetchAttractions();
+    }
+}
+
+function handleSearchSubmit(event) {
+    event.preventDefault(); 
+    keyword=document.getElementById('keyword').value.trim(); 
+    page=0; 
+    const container=document.getElementById('attractions-container');
+    container.innerHTML=''; 
+    window.removeEventListener('scroll', handleScroll);
+    fetchAttractions(); 
+    window.addEventListener('scroll', handleScroll);
+}
+
+window.addEventListener('DOMContentLoaded', fetchAttractions);
+window.addEventListener('scroll', handleScroll);
+document.getElementById('searchForm').addEventListener('submit', handleSearchSubmit);
+
---
+++ static/mrtlist_handler.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', fetchMRTList);
+
+// 獲取 MRT 
+async function fetchMRTList(){
+    try {
+        const response=await fetch('/api/mrts');
+        const data=await response.json();
+        renderMRTList(data.data);
+    } catch (error) {
+        console.error('An error occurred while fetching MRT list:', error);
+    }
+}
+
+// 呈現 MRT 
+function renderMRTList(mrtNames){
+    const mrtListContainer=document.getElementById('mrt-list');
+    mrtListContainer.innerHTML='';
+
+    mrtNames.forEach(mrtName =>{
+        const mrtElement=document.createElement('div');
+        mrtElement.textContent=mrtName;
+        mrtElement.classList.add('mrt-station'); 
+        mrtElement.addEventListener('click', handleMRTClick);
+        mrtListContainer.appendChild(mrtElement);
+    });
+}
+
+// 左右滾動
+const leftButton=document.querySelector('.mrt-list-button-left');
+const rightButton=document.querySelector('.mrt-list-button-right');
+const mrtList=document.getElementById('mrt-list');
+
+leftButton.addEventListener('click', ()=>{
+    mrtList.scrollLeft-=mrtList.offsetWidth*0.8; 
+});
+
+rightButton.addEventListener('click', ()=>{
+    mrtList.scrollLeft+=mrtList.offsetWidth*0.8; 
+});
+
+// 點擊 MRT
+function handleMRTClick(event){
+    const container=document.getElementById('attractions-container');
+    container.innerHTML='';
+
+    const clickedMRT=event.target.textContent;
+    const keywordInput=document.getElementById('keyword');
+    keywordInput.value=clickedMRT;
+    page=0; 
+
+    handleSearchSubmit(new Event('submit'));
+}
+
