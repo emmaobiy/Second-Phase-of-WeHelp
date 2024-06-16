d46c0bc484c00402b919d5ff106a9eea2517c9c8
emmaobiy <obiy10@gmail.com>
2024-06-16 09:56:29 +0800

    (1) Modified attraction.html and related files. (2) Updated index-related files with 'index'. (3) Added link to homepage in website title. (4) Added link to attraction-page in index_attractions_handler.js. (5) Adjusted GROUP CONCAT MAX LEN in attractions_handler.py

--- static/index_attractions_handler.js
+++ static/index_attractions_handler.js
@@ -29,44 +29,52 @@ function addAttraction(attractions) {
         const attraction=attractions[i];
         const attractionExists=Container.querySelector(`[data-id="${attraction.id}"]`);
         if (!attractionExists) {
+
             // each-attraction
             const eachAttraction=document.createElement('div');
-            eachAttraction.classList.add('each-attraction');
+            eachAttraction.classList.add('index-each-attraction');
             eachAttraction.setAttribute('data-id', attraction.id);
             
             // attraction-imgContainer
             const imgContainer=document.createElement('div');
-            imgContainer.classList.add('attraction-imgContainer');
+            imgContainer.classList.add('index-attraction-imgContainer');
             
             // img
             const img=document.createElement('img');
             img.src=attraction.images[0];
             img.alt=attraction.name;
+
+            imgContainer.appendChild(img);
+            eachAttraction.appendChild(imgContainer);
         
             // attraction-name
             const name=document.createElement('div');
-            name.classList.add('attraction-name', 'font-body-bold16-white');
-            name.textContent=attraction.name;
-
+            name.classList.add('index-attraction-name', 'font-body-bold16-white');
+            name.setAttribute('data-id', attraction.id);
             imgContainer.appendChild(name);
-            imgContainer.appendChild(img);
-            eachAttraction.appendChild(imgContainer);
 
+            //link 
+            const link = document.createElement('a');
+            link.href = `/attraction/${attraction.id}`;
+            link.textContent=attraction.name;
+            name.appendChild(link);
+                     
             // attraction-info
             const info=document.createElement('div');
-            info.classList.add('attraction-info');
+            info.classList.add('index-attraction-info');
             
             const mrt=document.createElement('div');
-            mrt.classList.add('info-mrt','font-body-med16-gray');
+            mrt.classList.add('index-info-mrt','font-body-med16-gray');
             mrt.textContent=attraction.mrt;
             
             const cat=document.createElement('div');
-            cat.classList.add('info-cat', 'font-body-med16-gray');
+            cat.classList.add('index-info-cat', 'font-body-med16-gray');
             cat.textContent=attraction.cat;
         
             info.appendChild(mrt);
             info.appendChild(cat);
             eachAttraction.appendChild(info);
+
 
             Container.appendChild(eachAttraction);
         };
